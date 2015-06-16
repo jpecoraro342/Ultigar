52b44e8beab002203dffab360f8c0c5514049d0c
jpecoraro342 <jpecoraro342@ufl.edu>
2015-06-15 21:01:26 -0400

    Added mini-cell generation

---
+++ Assets/Scripts/AI/MiniCellColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniCellColor : MonoBehaviour {
+
+	private Color color;
+	
+	void Start () {
+		color = new Color(Random.value, Random.value, Random.value);
+		gameObject.GetComponent<Renderer>().material.color = color;
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Utilities/MiniCellManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniCellManager : MonoBehaviour {
+
+	public int maxNumberOfCells = 1000;
+	public GameObject cellParent;
+	public GameObject miniCellPrefab;
+	
+	void Start () {
+		InitializeMiniCells();
+	}
+
+	void InitializeMiniCells() {
+		for (int i = 0; i < maxNumberOfCells; i++) {
+			CreateCell();
+		}
+	}
+
+	void CreateCell() {
+		Vector3 position = new Vector3(Random.Range(-50.0F, 50.0F), Random.Range(-50.0F, 50.0F), 0);
+		GameObject miniCell = (GameObject)Instantiate(miniCellPrefab, position, Quaternion.identity);
+		miniCell.transform.parent = cellParent.transform;
+	}
+}
